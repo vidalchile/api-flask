eb8cd4419e2a4008be7e24d52c656831d058b1f6
Cristian Vidal <cris.vidal04@gmail.com>
2019-12-01 22:09:41 -0300

    endpoint para editar tarea

--- app/views.py
+++ app/views.py
@@ -10,19 +10,28 @@ api_v1 = Blueprint('api', __name__, url_prefix='/api/v1')
 
 @api_v1.route('/tasks', methods=['GET'])
 def get_tasks():
+    
+    # Obtener todos los objetos
     tasks = Task.query.all() # select * from tasks;
     list_tasks = [ task.serialize() for task in tasks]
+
     return response(list_tasks)
 
 @api_v1.route('/tasks/<id>', methods=['GET'])
 def get_task(id):
+
+    # Obtener objeto segun su ID
     task =  Task.query.filter_by(id=id).first()
+    
+    # Validar existencia objeto
     if task is None:
         return (not_found())
+    
     return response(task.serialize())
 
 @api_v1.route('/tasks', methods=['POST'])
 def create_tasks():
+
     # Objeto json que el cliente nos envia
     json  = request.get_json(force=True)
 
@@ -46,8 +55,28 @@ def create_tasks():
     return bad_request()
 
 @api_v1.route('/tasks/<id>', methods=['PUT'])
-def update_tasks():
-    pass
+def update_tasks(id):
+    
+    # Obtener objeto
+    task = Task.query.filter_by(id=id).first()
+
+    # Validar existencia objeto
+    if task is None:
+        return (not_found())
+
+    # Obtener json cliente
+    json = request.get_json(force=True)
+
+    # Si el cliente no envia un valor para titulos seguira el mismo
+    task.title = json.get('title', task.title)
+    task.description = json.get('description', task.description)
+    task.deadline = json.get('deadline', task.deadline)
+
+    # Persistir cambios
+    if task.save():
+        return response(task.serialize())
+
+    return bad_request()
 
 @api_v1.route('/tasks/<id>', methods=['DELETE'])
 def delete_tasks():
